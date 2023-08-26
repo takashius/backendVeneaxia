bc9eaa10e93cfa8cdb540834d8431718e63a1821
Erick Hernandez <takashi.onimaru@gmail.com>
2023-08-26 14:00:02 -0400

    Configuracion base de swagger

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ import db from "./db.js";
 import config from "./config.js";
 import router from "./network/routes.js";
 import cors from "cors";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 db(config.dbUrl);
 
@@ -12,6 +19,24 @@ server.use(bodyParser.json());
 server.use(cors());
 
 router(server);
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: "1.0.0",
+    title: "Codu API",
+    description: "Prueba inicial",
+    contact: {
+      name: "Erick Hernandez",
+      url: "https://erdesarrollo.com.ve",
+    },
+    servers: [`http://localhost:${config.port}`],
+  },
+  apis: [`${path.join(__dirname, "./components/user/network.js")}`],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 server.use(_static(config.publicRoute));
 server.use(_static("./static"));
 
